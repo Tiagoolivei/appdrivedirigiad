990694205db216c50370a5ecd0727db3669b0f51
Tiagoolivei <87621874+Tiagoolivei@users.noreply.github.com>
2022-06-08 06:37:46 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,9 +1,9 @@
 <?php 
 
 $host="localhost";     /* conectar ao servidor*/
-$user="";         /* loguin da conta no servidor*/
-$pass="";             /*senha da conta no servidor*/
-$banc="";     /*nome do banco criado*/   
+$user="id19053810_appmobilidade";         /* loguin da conta no servidor*/
+$pass="Passaro@10@10";             /*senha da conta no servidor*/
+$banc="id19053810_cadastromobilidade";     /*nome do banco criado*/   
 
 $conectar = mysqli_connect($host, $user, $pass, $banc);
 
